ded1d02f6ac7cafaf0f32a7a5fe8b90567da41ca
Daniel <d.le@student.tue.nl>
2024-03-10 21:21:05 +0100

    Implemented Tester.

--- main.py
+++ main.py
@@ -10,16 +10,42 @@ Utrecht University & University of Technology Eindhoven
 from dataset import prostateMRDataset
 from utils import load_config
 from models import u_net, vae
-from train import Trainer
+from train_val_test import Trainer, Tester
 
 config = load_config("config.json")
 train_loader, val_loader = prostateMRDataset(config)
-model = u_net.UNet()
-# model = vae.VAE()
-#%%
-trainer = Trainer(model=model,
-                  train_loader=train_loader,
-                  val_loader=val_loader,
-                  config=config)
-trainer.train()
+print(f"Train: {len(train_loader)} | Val: {len(val_loader)}")
 
+#%% ========== Train ==========
+# model = u_net.UNet()
+# model = vae.VAE()
+# trainer = Trainer(model=model,
+#                   train_loader=train_loader,
+#                   val_loader=val_loader,
+#                   config=config)
+
+# trainer.train()
+
+# %% ========== Generate images ==========
+model = vae.VAE()
+vae_test = Tester(model=model,
+                  weights_path=R"vae_results\20240310_160304_epochs800_lr0.001_decay500\weights.pth",
+                  config=config)
+images = vae_test.evaluate()
+vae_test.plot_images()
+
+# %% ========== Segmentation ==========
+model = u_net.UNet()
+unet_test = Tester(model=model,
+                   weights_path=R"segmentation_results\20240309_232510\weights.pth",
+                   config=config,
+                   test_loader=val_loader)
+
+images = unet_test.evaluate()
+unet_test.plot_images()
+#%%
+scores = unet_test.calc_scores()
+# %%
+# TODO:
+# Images blurry -> disentangle latent vectors -> Beta-VAE -> add beta factor to KL loss term?
+# Generate corresponding masks -> concatenate dim=1, channel dim?
